2c098608feb482e289279d4c886aeed969485725
yasaswinibalivada <141722903+yasaswinibalivada@users.noreply.github.com>
2024-04-16 14:56:23 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "VIZAG",
+        hint: "The City of Destiny"
+    },
+    {
+        word: "JAIPUR",
+        hint: "The Pink City of India"
+    },
+    {
+        word: "HYDERABAD",
+        hint: "City of Pearls"
+    },
+    {
+        word: "LUCKNOW",
+        hint: "City of Nawabs"
+    },
+    {
+        word: "AGRA",
+        hint: "The City where Taj Mahal is Located"
+    },
+    {
+        word: "NEWDELHI",
+        hint: "Capital City of India"
+    },
+    {
+        word: "PONDICHERRY",
+        hint: "Paris of East"
+    },
+    {
+        word: "MUMBAI",
+        hint: "City of Dreams"
+    },
+    {
+        word: "KOLKATA",
+        hint: "City of Joy"
+    },
+    {
+        word: "JODHPUR",
+        hint: "Blue City of India"
+    },
+    {
+        word: "GOA",
+        hint: "Party Capital"
+    },
+    {
+        word: "DARJEELING",
+        hint: "Queen of Hills"
+    },
+    {
+        word: "GUNTUR",
+        hint: "Spice Capital"
+    },
+    {
+        word: "KOCHI",
+        hint: "Queen of Arabian Sea"
+    },
+    {
+        word: "MYSORE",
+        hint: "Sandalwood City of India"
+    },
+    {
+        word: "UDAIPUR",
+        hint: "City of Lakes"
+    },
+    {
+        word: "COORG",
+        hint: "Scotland of India"
+    },
+    {
+        word: "KASHMIR",
+        hint: "Switzerland of India"
+    },
+    {
+        word: "MADURAI",
+        hint: "India's Temple City"
+    },
+    {
+        word: "MATHURA",
+        hint: "Lord Krishna's Birth Place"
+    },
+    {
+        word: "MADRAS",
+        hint: "Chennai is also known as"
+    },
+    {
+        word: "GANGTOK",
+        hint: "City's Name means 'Top of the Hill'"
+    },
+    {
+        word: "CUTTACK",
+        hint: "Silver City of India"
+    },
+]
